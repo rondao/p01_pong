fe38739e5ef9f6141102ad03cf042c45674e60df
Rafael Rondao <rafael.rondao@gmail.com>
2020-06-11 14:13:31 -0300

    Create Human player type to be played by input
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- paddle.gd
+++ paddle.gd
@@ -1,16 +1,24 @@
 extends KinematicBody2D
 
+enum Player {HUMAN, NONE}
+export(Player) var player
+
 export(int) var speed
 var velocity = Vector2()
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
+	match player:
+		Player.HUMAN:
+			_handle_input()
+	
+# warning-ignore:return_value_discarded
+	move_and_slide(velocity)
+
+func _handle_input():
 	if Input.is_action_pressed("ui_up"):
 		velocity.y = -speed
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y = speed
 	else:
 		velocity = Vector2.ZERO
-	
-# warning-ignore:return_value_discarded
-	move_and_slide(velocity)
