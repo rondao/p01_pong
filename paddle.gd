38812a4a0562141f319a181133d19e42728c63e9
Rafael Rondao <rafael.rondao@gmail.com>
2020-06-20 11:45:59 -0300

    Reset charge upon hitting the ball
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- paddle.gd
+++ paddle.gd
@@ -11,15 +11,19 @@ var velocity = Vector2()
 var charge = 0.0
 var _max_charge = 0.5
 
-# warning-ignore:unused_argument
 func _physics_process(delta):
 	match player:
 		Player.HUMAN_01:
 			_handle_input("player_01", delta)
 		Player.HUMAN_02:
 			_handle_input("player_02", delta)
-	
-	velocity = move_and_slide(velocity)
+
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if collision.collider.is_in_group("ball"):
+			_collide_ball()
+		elif collision.collider.is_in_group("walls"):
+			_collide_walls()
 
 func _handle_input(player_number, delta):
 	if Input.is_action_pressed(player_number + "_up"):
@@ -34,3 +38,9 @@ func _handle_input(player_number, delta):
 		velocity.y *= 1.0 - charge
 	else:
 		charge = 0.0
+
+func _collide_ball():
+	charge = 0.0
+
+func _collide_walls():
+	velocity = Vector2.ZERO
