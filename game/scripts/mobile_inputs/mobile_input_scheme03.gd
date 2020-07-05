0511dc705785ede1e905ed5f4ba57b31f2cd0374
Rafael Rondao <rafael.rondao@gmail.com>
2020-07-05 17:20:52 -0300

    Create mobile inputs for one hand
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

---
+++ game/scripts/mobile_inputs/mobile_input_scheme03.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+## Mobile Input Scheme 03
+#
+# This scheme uses only one screen side,
+#  required for one device multiplayer.
+#
+# Paddle will move to touch y-position.
+# Dragging in X direction activates charge.
+#
+
+onready var _center := Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
+
+const DRAG_FOR_CHARGE := 100
+var _touch_position := Vector2.ZERO
+
+func _input(event: InputEvent):
+	if event is InputEventScreenDrag:
+		_drag_to_action(event as InputEventScreenDrag)
+	elif event is InputEventScreenTouch:
+		_touch_to_action(event as InputEventScreenTouch)
+
+
+func _drag_to_action(drag: InputEventScreenDrag):
+	Input.action_press("player_01_move_to", drag.position.y / get_viewport().size.y)
+	
+	if abs(drag.position.x - _touch_position.x) > DRAG_FOR_CHARGE:
+		Input.action_press("player_01_charge")
+
+
+func _touch_to_action(touch: InputEventScreenTouch):
+	if touch.is_pressed():
+		_touch_position = touch.position
+		Input.action_press("player_01_move_to", touch.position.y / get_viewport().size.y)
+	else:
+		Input.action_release("player_01_move_to")
+		Input.action_release("player_01_charge")
