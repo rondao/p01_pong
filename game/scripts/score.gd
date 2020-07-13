29de4b18d2a0cf242e1b9ee7df4388d0b9c78ec6
Rafael Rondao <rafael.rondao@gmail.com>
2020-07-13 12:57:24 -0300

    Highlight score text when increasing it
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- game/scripts/score.gd
+++ game/scripts/score.gd
@@ -1,8 +1,12 @@
 extends Label
 
+onready var _animation := $Animation as AnimationPlayer
+const HIGHLIGHT_TEXT_ANIMATION := "highlight_text"
+
 
 func _increase_score():
 	text = str(int(text) + 1)
+	_animation.play("highlight_text")
 
 
 func _on_Ball_left_scored():
