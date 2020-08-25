477b438cb52259f0d997b277dcbd44c167a9255d
Rafael Rondao <rafael.rondao@gmail.com>
2020-08-25 19:01:43 -0300

    Show popup after game end
    May show information about the match.
    Quit back to the main menu.
    
    Pause the game when popup is opened.
    Changed goal sfx to never pause so it is animated on last goal.
    
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

---
+++ game/scripts/end_game_popup.gd
@@ -0,0 +1,5 @@
+extends PopupPanel
+
+
+func _on_Quit_pressed():
+	hide()
--- game/scripts/pong_game.gd
+++ game/scripts/pong_game.gd
@@ -4,28 +4,28 @@ class_name PongGame
 signal left_scored()
 signal right_scored()
 
-export(PackedScene) var MobileInputOneHand: PackedScene
-export(PackedScene) var MobileInputTwoHands: PackedScene
-
 onready var _left_paddle := $LeftPaddle as Paddle
 onready var _right_paddle := $RightPaddle as Paddle
 
 onready var _left_goal := $LeftGoal as PhysicsBody2D
 onready var _right_goal := $RightGoal as PhysicsBody2D
 
+onready var _ball := $Ball as Node2D
+
 enum GameType {NONE, LOCAL_AI, LOCAL_MULTIPLAYER, NETWORK_MULTIPLAYER, SERVER}
 var _game_type: int = GameType.NONE
 
-var _network_side: int
+var player_side: int
 
+const goals_to_win := 2
 var _left_score := 0
 var _right_score := 0
 
 
-static func create_game(game_type: int, network_side: int = Globals.Side.NONE) -> PongGame:
+static func create_game(game_type: int, _player_side: int = Globals.Side.LEFT) -> PongGame:
 	var game := (load("res://game/scenes/pong_game.tscn") as PackedScene).instance() as PongGame
 	game._game_type = game_type
-	game._network_side = network_side
+	game.player_side = _player_side
 	return game
 
 
@@ -34,12 +34,12 @@ func _ready():
 		var mobile_input
 		match UserPreferences.mobile_input:
 			Globals.MobileInput.ONE_HAND:
-				mobile_input = MobileInputOneHand.instance()
+				mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_one_hand.tscn") as PackedScene).instance()
 			Globals.MobileInput.TWO_HANDS:
-				mobile_input = MobileInputTwoHands.instance()
+				mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_two_hands.tscn") as PackedScene).instance()
 
 		if Network.is_network_game():
-			mobile_input.set_side(_network_side)
+			mobile_input.set_side(player_side)
 
 		add_child(mobile_input)
 
@@ -68,7 +68,7 @@ func _configure_game_as_server():
 
 
 func _configure_game_as_network_multiplayer():
-	match _network_side:
+	match player_side:
 		Globals.Side.LEFT:
 			_left_paddle.player_type = Paddle.PlayerType.HUMAN_01
 			_right_paddle.player_type = Paddle.PlayerType.NETWORK
@@ -97,7 +97,7 @@ func _configure_game_as_local_ai():
 
 func _process(_delta: float):
 	if _game_type == GameType.NETWORK_MULTIPLAYER:
-		match _network_side:
+		match player_side:
 			Globals.Side.LEFT:
 				_left_paddle.rpc_unreliable("set_position", _left_paddle.position)
 			Globals.Side.RIGHT:
@@ -112,3 +112,41 @@ func _on_Ball_collided_goal(side: int):
 		Globals.Side.RIGHT:
 			_right_score += 1
 			emit_signal("right_scored", _right_score)
+
+	if _left_score == goals_to_win:
+		match player_side:
+			Globals.Side.LEFT:
+				_popup_end_game(true)
+			Globals.Side.RIGHT:
+				_popup_end_game(false)
+		_end_game()
+	elif _right_score == goals_to_win:
+		match player_side:
+			Globals.Side.LEFT:
+				_popup_end_game(false)
+			Globals.Side.RIGHT:
+				_popup_end_game(true)
+		_end_game()
+
+
+func _end_game():
+	get_tree().set_pause(true)
+
+
+func _popup_end_game(_won: bool):
+	var end_game_popup := (load("res://game/scenes/end_game_popup.tscn") as PackedScene).instance() as PopupPanel
+	get_tree().get_root().add_child(end_game_popup)
+
+	end_game_popup.connect("hide", self, "_on_EndGamePopup_hide")
+	end_game_popup.popup()
+
+
+func _on_EndGamePopup_hide():
+	get_tree().set_pause(false)
+
+	var main_menu := (load("res://game/scenes/main_menu.tscn") as PackedScene).instance()
+
+	get_tree().get_root().add_child(main_menu)
+	get_tree().get_root().remove_child(self)
+
+	self.queue_free()
