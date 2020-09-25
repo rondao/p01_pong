0e5216ccc86032836cd53cfb52c19402f8f48c5c
Rafael Rondao <rafael.rondao@gmail.com>
2020-09-25 12:18:28 -0300

    Change left and right paddle to my and opponent
    This reduce duplicated code
    
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- game/scripts/pong_game.gd
+++ game/scripts/pong_game.gd
@@ -4,9 +4,6 @@ class_name PongGame
 signal left_scored()
 signal right_scored()
 
-onready var _left_paddle := $LeftPaddle as Paddle
-onready var _right_paddle := $RightPaddle as Paddle
-
 onready var _left_goal := $LeftGoal as PhysicsBody2D
 onready var _right_goal := $RightGoal as PhysicsBody2D
 
@@ -14,6 +11,9 @@ onready var _ball := $Ball as Ball
 
 enum GameType {NONE, LOCAL_AI, LOCAL_MULTIPLAYER, NETWORK_MULTIPLAYER, SERVER}
 var _game_type: int = GameType.NONE
+
+var _my_paddle: Paddle
+var _opponent_paddle: Paddle
 
 var player_side: int
 
@@ -31,17 +31,15 @@ static func create_game(game_type: int, _player_side: int = Globals.Side.LEFT) -
 
 func _ready():
 	if OS.has_touchscreen_ui_hint():
-		var mobile_input
-		match UserPreferences.mobile_input:
-			Globals.MobileInput.ONE_HAND:
-				mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_one_hand.tscn") as PackedScene).instance()
-			Globals.MobileInput.TWO_HANDS:
-				mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_two_hands.tscn") as PackedScene).instance()
+		_add_touchscreen_input()
 
-		if get_tree().network_peer != null:
-			mobile_input.set_side(player_side)
-
-		add_child(mobile_input)
+	match player_side:
+		Globals.Side.LEFT:
+			_my_paddle = $LeftPaddle as Paddle
+			_opponent_paddle = $RightPaddle as Paddle
+		Globals.Side.RIGHT:
+			_opponent_paddle = $LeftPaddle as Paddle
+			_my_paddle = $RightPaddle as Paddle
 
 	match _game_type:
 		GameType.SERVER:
@@ -54,18 +52,32 @@ func _ready():
 			_configure_game_as_local_ai()
 
 
+func _add_touchscreen_input():
+	var mobile_input
+	match UserPreferences.mobile_input:
+		Globals.MobileInput.ONE_HAND:
+			mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_one_hand.tscn") as PackedScene).instance()
+		Globals.MobileInput.TWO_HANDS:
+			mobile_input = (load("res://game/scenes/mobile_inputs/mobile_input_two_hands.tscn") as PackedScene).instance()
+
+	if GameServer.is_network_game():
+		mobile_input.set_side(player_side)
+
+	add_child(mobile_input)
+
+
 func _configure_game_as_server():
 	get_tree().connect("network_peer_disconnected", self, "_on_Network_disconnected")
 	get_tree().connect("server_disconnected", self, "_on_Network_disconnected")
 
-	_left_paddle.player_type = Paddle.PlayerType.NETWORK
-	_left_paddle.set_collision_layer(0)
-	_left_paddle.set_collision_mask(0)
+	_my_paddle.player_type = Paddle.PlayerType.NETWORK
+	_my_paddle.set_collision_layer(0)
+	_my_paddle.set_collision_mask(0)
 	_left_goal.set_collision_layer(0)
 	_left_goal.set_collision_mask(0)
-	_right_paddle.player_type = Paddle.PlayerType.NETWORK
-	_right_paddle.set_collision_layer(0)
-	_right_paddle.set_collision_mask(0)
+	_opponent_paddle.player_type = Paddle.PlayerType.NETWORK
+	_opponent_paddle.set_collision_layer(0)
+	_opponent_paddle.set_collision_mask(0)
 	_right_goal.set_collision_layer(0)
 	_right_goal.set_collision_mask(0)
 
@@ -75,56 +87,41 @@ func _configure_game_as_network_multiplayer():
 	get_tree().connect("server_disconnected", self, "_on_Network_disconnected")
 	GameServer._socket.connect("received_match_state", self, "_on_NakamaSocket_received_match_state")
 
+	_my_paddle.player_type = Paddle.PlayerType.HUMAN_01
+	_opponent_paddle.player_type = Paddle.PlayerType.NETWORK
+	_opponent_paddle.set_collision_layer(0)
+	_opponent_paddle.set_collision_mask(0)
+
 	match player_side:
 		Globals.Side.LEFT:
-			_left_paddle.player_type = Paddle.PlayerType.HUMAN_01
-			_right_paddle.player_type = Paddle.PlayerType.NETWORK
-			_right_paddle.set_collision_layer(0)
-			_right_paddle.set_collision_mask(0)
 			_right_goal.set_collision_layer(0)
 			_right_goal.set_collision_mask(0)
 		Globals.Side.RIGHT:
-			_right_paddle.player_type = Paddle.PlayerType.HUMAN_01
-			_left_paddle.player_type = Paddle.PlayerType.NETWORK
-			_left_paddle.set_collision_layer(0)
-			_left_paddle.set_collision_mask(0)
 			_left_goal.set_collision_layer(0)
 			_left_goal.set_collision_mask(0)
 
 
 func _configure_game_as_local_multiplayer():
-	_left_paddle.player_type = Paddle.PlayerType.HUMAN_01
-	_right_paddle.player_type = Paddle.PlayerType.HUMAN_02
+	_my_paddle.player_type = Paddle.PlayerType.HUMAN_01
+	_opponent_paddle.player_type = Paddle.PlayerType.HUMAN_02
 
 
 func _configure_game_as_local_ai():
-	_left_paddle.player_type = Paddle.PlayerType.HUMAN_01
-	_right_paddle.player_type = Paddle.PlayerType.AI
+	_my_paddle.player_type = Paddle.PlayerType.HUMAN_01
+	_opponent_paddle.player_type = Paddle.PlayerType.AI
 
 
 func _on_Timer_timeout():
 	if _game_type == GameType.NETWORK_MULTIPLAYER:
-		match player_side:
-			Globals.Side.LEFT:
-				GameServer.send_paddle_position(_left_paddle.position)
-			Globals.Side.RIGHT:
-				GameServer.send_paddle_position(_right_paddle.position)
+		GameServer.send_paddle_position(_my_paddle.position)
 
 
 func _on_NakamaSocket_received_match_state(match_state: NakamaRTAPI.MatchData):
 	match match_state.op_code:
 		GameServer.OpCodes.SET_PADDLE_POSITION:
-			match player_side:
-				Globals.Side.LEFT:
-					_right_paddle.set_position(str2var(match_state.data))
-				Globals.Side.RIGHT:
-					_left_paddle.set_position(str2var(match_state.data))
+			_opponent_paddle.set_position(str2var(match_state.data))
 		GameServer.OpCodes.SET_PADDLE_CHARGE:
-			match player_side:
-				Globals.Side.LEFT:
-					_right_paddle.set_charge(str2var(match_state.data))
-				Globals.Side.RIGHT:
-					_left_paddle.set_charge(str2var(match_state.data))
+			_opponent_paddle.set_charge(str2var(match_state.data))
 		GameServer.OpCodes.BALL_COLLIDED_WITH_PADDLE:
 			var data: Dictionary = str2var(match_state.data)
 			_ball.apply_collision(data["position"], data["velocity"], data["bonus_velocity"], data["spin"])
